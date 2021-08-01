7fa4e325b53f10bb9c6b0c2ee799549b0b683feb
TheyKay <murraykyleb@gmail.com>
2021-07-31 18:00:26 -0600

    styling resume page

--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,13 +1,13 @@
 <template>
   <h1>Resume</h1>
+  <!-- <img v-bind:src="Resume" alt="pdf of my resume" /> -->
 </template>
 
 <script>
+// import Resume from '../assets/images/kayMurrayResume.pdf';
 export default {
-  name: 'Resume'
-}
+  name: "Resume",
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
