e3d670605622fb566874e5bffa7da3d227d4e50b
TheyKay <murraykyleb@gmail.com>
2021-08-02 13:58:01 -0600

    added download icon to resume button

--- src/views/About.vue
+++ src/views/About.vue
@@ -21,7 +21,21 @@
       </p>
     </section>
     <div className="in-page-link">
-      <a href="./images/kayMurrayResume.pdf" download>resume</a>
+      <a href="./images/kayMurrayResume.pdf" download>
+        <svg
+          width="18"
+          height="20"
+          viewBox="0 0 18 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M9 0C8.58579 0 8.25 0.335786 8.25 0.75V11.8747L5.69797 9.32272C5.37984 9.00458 4.86403 9.00458 4.54589 9.32272C4.22776 9.64086 4.22776 10.1567 4.54589 10.4748L8.45951 14.3884C8.77765 14.7066 9.29345 14.7066 9.61159 14.3884C9.71125 14.2888 9.77969 14.1697 9.81691 14.0434L13.2637 10.5966C13.5819 10.2784 13.5819 9.76264 13.2637 9.4445C12.9456 9.12636 12.4298 9.12636 12.1116 9.4445L9.75 11.8061V0.75C9.75 0.335786 9.41421 0 9 0ZM0.75 17.7083C0.335786 17.7083 0 18.0441 0 18.4583C0 18.8725 0.335787 19.2083 0.75 19.2083H17.25C17.6642 19.2083 18 18.8725 18 18.4583C18 18.0441 17.6642 17.7083 17.25 17.7083H0.75Z"
+            fill="#909A9C"
+          />
+        </svg>
+        resumé
+      </a>
     </div>
   </section>
 </template>
@@ -55,12 +69,11 @@ p {
   }
 }
 a {
-  display: block;
-  
+  display: inline-block;
   color: styles.$darkpink;
-  padding: .25rem;
+  padding: 0.5rem;
   border: 2px solid styles.$empink;
-  border-radius: 1rem;
+  border-radius: 0.5rem;
   line-height: 1;
   &:link {
     background-color: white;
@@ -69,7 +82,7 @@ a {
     background-color: white;
   }
   &:hover {
-    background-color: white;
+    background-color: styles.$empink;
   }
   &:active {
     background-color: white;
