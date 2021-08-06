cd8ceaed51105e0e6216d0837c7f346857ac8412
TheyKay <murraykyleb@gmail.com>
2021-08-06 15:03:20 -0600

    put link to home page in header, only visible on pages that are not home

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,12 +3,15 @@
     <h1 className="site-name">theyKay Design</h1>
     <h1 className="location">{{ title }}</h1>
   </header>
+  <div v-if="notHome" className="in-page-link">
+    <router-link to="/">home</router-link>
+  </div>
 </template>
 
 <script>
-import { computed } from '@vue/runtime-core';
+import { computed } from "@vue/runtime-core";
 export default {
-  name: 'Header',
+  name: "Header",
   // props: {
   //   title: {
   //     type: String,
@@ -18,26 +21,35 @@ export default {
   computed: {
     title: function() {
       return this.$route.meta.title;
-    }
-  }
+    },
+    notHome: function() {
+      console.log(this.$route.meta.title);
+      return this.$route.meta.title != "";
+    },
+  },
 };
 </script>
 
 <style scoped lang="scss">
-  header {
-    width: calc(100vw - 2rem);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
-    font-size: 2rem;
-    background: styles.$pink;
-    color: styles.$darkpink;
-  }
-  .site-name {
-    text-align: left;
-  }
-  .location {
-    text-align: right;
-  }
+header {
+  width: calc(100vw - 2rem);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  font-size: 2rem;
+  background: styles.$pink;
+  color: styles.$darkpink;
+}
+.site-name {
+  text-align: left;
+}
+.location {
+  text-align: right;
+}
+.in-page-link {
+  background: styles.$pink;
+  color: styles.$darkpink;
+  padding: .5rem  0;
+}
 </style>
